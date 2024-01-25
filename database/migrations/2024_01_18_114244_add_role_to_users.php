ac1b52543f41e98b25b8df91883e4f3cd3219996
CojaONIX <cojaonix@gmail.com>
2024-01-25 12:12:32 +0100

    14.14 add role to users

--- database/migrations/2024_01_18_114244_add_role_to_users.php
+++ database/migrations/2024_01_18_114244_add_role_to_users.php
@@ -12,9 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            Schema::table('users', function (Blueprint $table) {
-                $table->string('role', 20)->default('user');
-            });
+            $table->string('role')->default('user');
         });
     }
 
