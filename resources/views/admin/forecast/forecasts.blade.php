ed99645628510d8669e0d09b34093ab8d4b08259
CojaONIX <cojaonix@gmail.com>
2024-01-26 13:01:44 +0100

    15.16 forecasts blade bootstrap

--- resources/views/admin/forecast/forecasts.blade.php
+++ resources/views/admin/forecast/forecasts.blade.php
@@ -7,38 +7,57 @@
     <form method="POST" action="{{ route('admin.forecasts.create') }}">
         @csrf
 
-        <select name="city_id">
-            @foreach($cities as $city)
-                <option value="{{ $city->id }}">{{ $city->city }}</option>
-            @endforeach
-        </select>
-        <input type="text" name="temperature" placeholder="Unesite temperaturu">
+        <div class="row col-6">
+            <div class="col-6">
+            <select name="city_id" class="form-select mb-3">
+                @foreach($cities as $city)
+                    <option value="{{ $city->id }}">{{ $city->city }}</option>
+                @endforeach
+            </select>
+            </div>
+            <div class="col-6">
+                <input type="text" name="temperature" class="form-control mb-3" placeholder="Unesite temperaturu">
+            </div>
+        </div>
 
-        <select name="weather_type">
-            @foreach(\App\Models\Forecast::WEATHERS as $weather)
-                <option value="{{ $weather }}">{{ $weather }}</option>
-            @endforeach
-        </select>
+        <div class="row col-md-6">
+            <div class="col-4">
+                <select name="weather_type" class="form-select mb-3">
+                    @foreach(\App\Models\Forecast::WEATHERS as $weather)
+                        <option value="{{ $weather }}">{{ $weather }}</option>
+                    @endforeach
+                </select>
+            </div>
 
-        <input type="text" name="probability" placeholder="% verovatnoce za padavine">
-        <input type="date" name="date">
+            <div class="col-4">
+                <input type="text" name="probability" class="form-control mb-3" placeholder="% verovatnoce za padavine">
+            </div>
+            <div class="col-4">
+                <input type="date" name="date" class="form-control mb-3" value="{{ \Illuminate\Support\Carbon::now()->format('Y-m-d') }}">
+            </div>
 
-        <button>Snimi</button>
+            <button class="btn btn-outline-primary col-12 my-3" type="submit">Save</button>
+        </div>
     </form>
 
+    <div class="d-flex flex-wrap">
     @foreach($cities as $city)
-        <h4>{{ $city->city }}</h4>
-        <ul>
-        @foreach($city->forecasts as $forecast)
+        <div class="mb-5 mx-2">
+            <h4>{{ $city->city }}</h4>
+            <ul class="list-group">
+            @foreach($city->forecasts as $forecast)
 
-            @php
-                $color = \App\Http\TemperatureColorHelper::getColorByTemperature($forecast->temperature)
-            @endphp
+                @php
+                    $color = \App\Http\TemperatureColorHelper::getColorByTemperature($forecast->temperature)
+                @endphp
 
-            <li>{{ $forecast->date }} - <span style="color:{{ $color }};">{{ $forecast->temperature }}</span> - {{ $forecast->weather_type }} - {{ $forecast->probability }}</li>
-        @endforeach
-        </ul>
+                <li class="list-group-item">{{ $forecast->date }} - <span style="color:{{ $color }};">{{ $forecast->temperature }}</span> - {{ $forecast->weather_type }} - {{ $forecast->probability }}</li>
+            @endforeach
+            </ul>
+        </div>
     @endforeach
+
+    </div>
 
 
 @endsection
